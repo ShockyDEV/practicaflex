e1efd9703375eb1ab087faa235ec18ca8d3b8f61
Shockynio <74144399+Shockynio@users.noreply.github.com>
2025-10-15 23:11:29 +0200

    Propiedades de margen y URL

--- main.c
+++ main.c
@@ -1,32 +1,49 @@
 #include <stdio.h>
 
+
 extern int yylex(void);
 extern FILE* yyin;
 
 extern int count_rules;
 extern int count_properties;
+extern int count_margins;
+extern int count_urls; 
 extern int longest_selector_len;
 extern char longest_selector[];
 
 int main(int argc, char** argv) {
+    
     if (argc > 1) {
-        yyin = fopen(argv[1], "r");
-        if (!yyin) {
+        
+        if (fopen_s(&yyin, argv[1], "r") != 0) {
             perror("Error al abrir el archivo");
             return 1;
         }
     }
     else {
+        
         yyin = stdin;
     }
 
     printf("Comprobando el archivo CSS...\n");
+
+    
     yylex();
 
-    printf("Reglas: %d\n", count_rules);
-    printf("Propiedades: %d\n", count_properties);
-    printf("Selector mas largo (%d): %s\n", longest_selector_len, longest_selector_len ? longest_selector : "");
+    
+
+    printf("Resultados del analisis:\n");
+    printf("Reglas encontradas: %d\n", count_rules);
+    printf("Propiedades totales: %d\n", count_properties);
+    printf("Propiedades de margen: %d\n", count_margins);
+    printf("Propiedades de tipo URL: %d\n", count_urls);
+    printf("Selector mas largo (%d caracteres): %s\n", longest_selector_len, longest_selector_len ? longest_selector : "N/A");
     printf("Comprobacion terminada jeje.\n");
+
+    if (yyin != stdin) {
+        fclose(yyin);
+    }
 
     return 0;
 }
+
