d2a449691435bae669a7ab206037343e262ab279
Shockynio <74144399+Shockynio@users.noreply.github.com>
2025-10-15 22:18:52 +0200

    Añadidas las propiedades y reglas

--- main.c
+++ main.c
@@ -3,6 +3,11 @@
 extern int yylex(void);
 extern FILE* yyin;
 
+extern int count_rules;
+extern int count_properties;
+extern int longest_selector_len;
+extern char longest_selector[];
+
 int main(int argc, char** argv) {
     if (argc > 1) {
         yyin = fopen(argv[1], "r");
@@ -18,6 +23,10 @@ int main(int argc, char** argv) {
     printf("Comprobando el archivo CSS...\n");
     yylex();
 
+    printf("Reglas: %d\n", count_rules);
+    printf("Propiedades: %d\n", count_properties);
+    printf("Selector mas largo (%d): %s\n", longest_selector_len, longest_selector_len ? longest_selector : "");
     printf("Comprobacion terminada jeje.\n");
+
     return 0;
 }
